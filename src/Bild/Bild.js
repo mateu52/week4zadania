ab9ed52bef75d392be57cc74c3ac272f8bb75f9f
Mateusz Walter <wuoelte@gmail.com>
2021-08-26 11:03:00 +0200

    first commit

---
+++ src/Bild/Bild.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const fuction Bild(){
+    function Price(({value})=> {value}*)
+    return(
+        <form>
+            <input type="number" name="netto" placeholder="netto..."></input>
+            <select>
+                <option value="5">5%</option>
+                <option value="10">10%</option>
+                <option value="15">15%</option>
+                <option value="20">20%</option>
+            </select>
+            <button>Przelicz</button>
+        </form>
+    );
+}
+
+
+
+export default Bild;
