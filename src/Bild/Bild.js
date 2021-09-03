043f9cf5a2a4055afa3f69b8aa9e67de01f404a9
Mateusz Walter <wuoelte@gmail.com>
2021-09-03 21:51:08 +0200

    komponent klasowy

--- src/Bild/Bild.js
+++ src/Bild/Bild.js
@@ -1,27 +1,81 @@
 import React ,{useState} from 'react';
 
-function Bild(){
-    const [val, setVal] = useState("-");
-    const sum=val; 
-    const setValue=()=><div>
-        <h1>hello</h1>
-    </div>
+class Bild extends React.Component{
+    state={
+        name:'',
+        surname:'',
+        age:''
+    }
+    sendForm=(event) =>{
+        event.preventDefault();
+    }
+    sendClick=(event)=>{
+        console.log("React bott...");
 
-    return(
-        <div>
-            <input type="number" name="netto" placeholder="netto..."></input>
-            <select>
-                <option value="5">5%</option>
-                <option value="10">10%</option>
-                <option value="15">15%</option>
-                <option value="20">20%</option>
-            </select>
-            <button onClick={ setValue }>Przelicz</button>
-            <h1>{setValue}</h1>
-        </div>
-    );
+    }
+    sendChange=(event)=>{
+        //const keyName= event.target.name;
+        this.setState({ [event.target.name ] : event.target.value});
+    }
+    render() {
+    
+        return(
+            <form onSubmit={ this.sendForm }>
+                <div>
+                    <input type="text" name="name" placeholder="Name" onChange={ this.sendChange } />
+                </div>
+                <div>
+                    <input type="text" name="surname" placeholder="Surname" onChange={ this.sendChange } />
+                </div>
+                <div>
+                    <input type="text" name="age" placeholder="Age" onChange={ this.sendChange } />
+                </div>
+                <button type="submit" onClick={ this.sendClick }>Send</button>
+            </form>
+            );
+    }
+        
 }
 
 
 
-export default Bild;
+
+/* function useInput(initialValue= ''){
+    const [value, setValue] = useState('ss');
+
+    const optionChange = (event) => {
+        setValue(event.target.value);
+    };
+
+    return [value, optionChange];
+}
+
+function Bild(){
+    const [netto, setNetto] = useInput('');
+    const [procent, setProcent] = useInput('a');
+
+    function sendForm(event){
+            event.preventDefault();
+            <p>{netto}</p>
+       
+    };
+
+    return(
+        <form onSubmit={ sendForm}>
+            <input type="number" onChange={ setNetto } name="netto" placeholder="netto..." />
+            <select  >
+                <option >wielkość napiwku(%)</option>
+                <option value="5"   onChange={useInput}>5%</option>
+                <option value="10"  onChange={setProcent}>10%</option>
+                <option value="15"  onChange={setProcent}>15%</option>
+                <option value="20"  onChange={setProcent}>20%</option>
+            </select>
+            <button type="submit">Przelicz</button>
+        </form>
+       
+    );
+}
+*/
+
+
+export default Bild; 
