1a06a86f70762ff66141f9868411ae868a351390
Mateusz Walter <wuoelte@gmail.com>
2021-09-04 07:42:08 +0200

    wlasny hook, komp funkcyjny

--- src/Bild/Bild.js
+++ src/Bild/Bild.js
@@ -1,5 +1,46 @@
 import React ,{useState} from 'react';
 
+
+function useInput(initialValue=''){
+    const [value, setValue]=useState('');
+
+    const sendChange=(event)=>{
+        setValue(event.target.value);
+    };
+
+    return [value, sendChange];
+}
+
+function Bild(){
+
+    const [name, setName]=useInput('');
+    const [surname, setSurname]=useInput('');
+    const [age, setAge]=useInput('');
+
+
+    const sendClick=()=>console.log('React butt..');
+    const sendForm=(event)=>{
+        event.preventDefault();
+    }
+
+    return(
+        <form onSubmit={ sendForm }>
+                <div>
+                    <input type="text" name="name" placeholder="Name" onChange={ setName } />
+                </div>
+                <div>
+                    <input type="text" name="surname" placeholder="Surname" onChange={ setSurname } />
+                </div>
+                <div>
+                    <input type="text" name="age" placeholder="Age" onChange={ setAge } />
+                </div>
+                <button type="submit" onClick={ sendClick }>Send</button>
+        </form>
+    );
+}
+
+
+/*
 class Bild extends React.Component{
     state={
         name:'',
@@ -35,47 +76,7 @@ class Bild extends React.Component{
             );
     }
         
-}
-
-
-
-
-/* function useInput(initialValue= ''){
-    const [value, setValue] = useState('ss');
-
-    const optionChange = (event) => {
-        setValue(event.target.value);
-    };
-
-    return [value, optionChange];
-}
-
-function Bild(){
-    const [netto, setNetto] = useInput('');
-    const [procent, setProcent] = useInput('a');
-
-    function sendForm(event){
-            event.preventDefault();
-            <p>{netto}</p>
-       
-    };
-
-    return(
-        <form onSubmit={ sendForm}>
-            <input type="number" onChange={ setNetto } name="netto" placeholder="netto..." />
-            <select  >
-                <option >wielkość napiwku(%)</option>
-                <option value="5"   onChange={useInput}>5%</option>
-                <option value="10"  onChange={setProcent}>10%</option>
-                <option value="15"  onChange={setProcent}>15%</option>
-                <option value="20"  onChange={setProcent}>20%</option>
-            </select>
-            <button type="submit">Przelicz</button>
-        </form>
-       
-    );
-}
-*/
+} */
 
 
 export default Bild; 
