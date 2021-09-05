ba14a42d19498e53547a7e7b526d34cce7a4551e
Mateusz Walter <wuoelte@gmail.com>
2021-09-05 10:32:44 +0200

    zad.1 komponent FUNKCYJNY formularz NAPIWEK

--- src/Bild/Bild.js
+++ src/Bild/Bild.js
@@ -1,6 +1,34 @@
 import React, { useState } from 'react';
 
-class Bild extends React.Component {
+function Bild(){
+    const [form, sendForm] = useState('');
+    const [netto, sendNetto] = useState('');
+    const [input, sendInput] = useState('');
+    const [brutto, sendBrutto] = useState('');
+
+    return(
+        <div>
+            <form onSubmit={sendForm}>
+                <div><h5>Podaj Kwote:</h5>
+                    <input type="number" name="netto" placeholder="netto" onClick={sendNetto}></input>
+                </div>
+                <div><h6>napiwek:
+                    <select  onClick={sendInput}>
+                        <option value='0.05' type="number" name="gratuity">5%</option>
+                        <option value='0.10' type="number" name="gratuity">10%</option>
+                        <option value='0.15' type="number" name="gratuity">15%</option>
+                        <option value='0.20' type="number" name="gratuity">20%</option>
+                    </select>
+                </h6></div>
+                <div>
+                    <button type="button" onClick={sendBrutto} name="brutto">Przelicz</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+/* class Bild extends React.Component {
     state={
         netto:0,
         gratuity:0,
@@ -41,5 +69,5 @@ class Bild extends React.Component {
         )
     }
 }
-
+ */
 export default Bild;
