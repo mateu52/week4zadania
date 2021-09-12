c35e8a056527aaef922075aee84b690bb28cc0b9
Mateusz Walter <wuoelte@gmail.com>
2021-09-12 10:57:10 +0200

    zadanie 3 -useForm-

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Bild from './Bild/Bild';
 import Dataform from './Dataform/Dataform';
+import ModDataform from './ModDataForm/ModDataForm';
 
 function App() {
   return (
      //<Bild />
-    <Dataform />
+    <ModDataform />
   );
 }
 
--- src/ModDataForm/ModDataForm.js
+++ src/ModDataForm/ModDataForm.js
@@ -1,43 +1,14 @@
-import React, {useState, useRef} from 'react';
+import { useForm } from "react-hook-form";
+import React, {useState } from 'react';
 
-function useCheck(initialValue){
-    const [value, setValue]=useState(initialValue);
+function ModDataform(){
 
-    const reset = () => {
-        setValue(initialValue);
-    }
+    const {register, handleSubmit} = useForm();
+    const sendForm = (d) =>
+        alert(JSON.stringify(d));
 
-    const sendChange=(event)=>{
-        setValue(event.target.value);
-    };
-
-    return [value, sendChange, reset];
-}
-
-
-function Dataform(){
-
-    const offName= useRef();
-    const offEmail= useRef();
-    const offBio = useRef();
-    const offGender = useRef();
-    const offCheck = useRef();
-    const checkboxMessage = useRef();
-    const genderMessage =useRef();
-
-    const [name, setName, resetName] =useCheck("");
-    const [email, setEmail, resetEmail] =useCheck("");
-    const [bio, setBio, resetBio] =useCheck('');
-    const [gender, setGender, resetGender] =useCheck('');
-    const [reguls, setReguls] =useState(false);
-    //const [submit, setSubmit] =useState('');
     const [radio1, setRadio1] =useState(false);
     const [radio2, setRadio2] =useState(false);
-
-
-    const sendForm=(event)=>{
-        event.preventDefault();
-    }
 
     const handleRadio1 =() =>{
         setRadio1(true);
@@ -47,128 +18,48 @@ function Dataform(){
         setRadio1(false);
         setRadio2(true);
     };
-    const onReguls=()=>{
-        setReguls(true);
-    }
-
-    const sendSubmit=(event)=>{
-        if(offName.current.value===''){
-            offName.current.focus();
-            offName.current.style.border='#f00 1px solid';
-            offName.current.value='Pole wymagane';
-        }else{
-            offName.current.style.color='black';
-        }
-        if(offEmail.current.value===''){
-            offEmail.current.focus();
-            offEmail.current.style.border='#f00 1px solid';
-            offEmail.current.value='Pole wymagane';
-        }else{
-            offEmail.current.style.color='black';
-        }
-        if(offBio.current.value===''){
-            offBio.current.focus();
-            offBio.current.style.border='#f00 1px solid';
-            offBio.current.value='Pole wymagane';
-        }else{
-            offBio.current.style.color='black';
-        }
-        if(gender ===""){
-            offGender.current.style.border = "2px solid red";
-            genderMessage.current.textContent="Podaj płeć";
-        }
-        else{
-            offGender.current.style.border='none';
-        }
-        if (reguls === false){
-            offCheck.current.style.border = "2px solid red";
-            checkboxMessage.current.textContent =
-            "Potwierdź zapoznanie się z regulaminem!!";
-        }
-        else{
-            offCheck.current.style.border='none';
-        }
-        if(
-            name !== '' &&
-            email !== '' &&
-            bio !== '' &&
-            (radio1 || radio2) ===true &&
-            reguls === true 
-        ){
-            alert("Dziekujemy za wysłanie");
-            resetName('');
-            resetEmail('');
-            resetBio('');
-            resetGender("");
-            setRadio1(false);
-            setRadio2(false);
-            setReguls(false);
-        }
-    };
 
     return(
-        <form onSubmit={sendForm}>
+        <form onSubmit={handleSubmit(sendForm)}>
             <div>
-                <input
-                        placeholder="imie"  
-                        type="text"
-                        value={name}
-                        ref={offName} 
-                        onChange={setName}  >
-                        </input>
+                <input {...register("name")} >
+                </input>
             </div><div>
-                <input
-                        placeholder="email" 
-                        type="text"
-                        value={email}
-                        ref={offEmail} 
-                        onChange={setEmail} >
-                        </input>
+                <input {...register("email")} >
+                </input>
             </div><div>
-                <textarea 
-                        onChange={setBio} 
-                        ref={offBio} 
-                        value={bio}
-                        type="text"
-                        placeholder="BIO"
-                        >
+                <textarea {...register("textarea")}>
                 </textarea>
             </div>
-            <div ref={offGender}>
+            <div >
                 <label>
-                    <input 
-                            type="radio" 
-                            value="female"
+                    <input {...register("female")}
+                            type="radio"
+                            onClick={handleRadio1}
                             checked={radio1}
-                            onClick={handleRadio1} 
-                            onChange={setGender}
                     />
                     kobieta
                 </label>
                 <label>
-                    <input 
-                            type="radio" 
-                            value="male"
+                    <input {...register("male")}
+                            type="radio"
+                            onClick={handleRadio2}
                             checked={radio2}
-                            onClick={handleRadio2} 
-                            onChange={setGender}
                     />
                     mezczyzna
                 </label>
-                <p ref={genderMessage}></p>
+                <p ></p>
             </div>
             <div>
-                <label ref={offCheck}><input 
-                    type="checkbox" 
-                    checked={reguls}
-                    onChange={onReguls}
+                <label><input {...register("reguls")}
+                    type="checkbox"
                     />
                     regulamin</label>
-                    <p ref={checkboxMessage} ></p>
+                    <p></p>
             </div><div>
-                <button type="button" 
-                    name="submit" 
-                    onClick={sendSubmit} >
+                <button type="submit"
+                    value="submit"
+                     >
                 Wyślij
                 </button>
             </div>
@@ -176,4 +67,4 @@ function Dataform(){
     );
 }
 
- export default Dataform;
+ export default ModDataform;
