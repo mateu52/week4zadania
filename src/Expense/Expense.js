b1fe5d55995fdb655342c86b30cec731078289f3
Mateusz Walter <wuoelte@gmail.com>
2021-09-23 12:30:01 +0200

    zad 4 uzuwany push brak sumy

--- src/Expense/Expense.js
+++ src/Expense/Expense.js
@@ -1,56 +1,76 @@
 import React, {useState} from 'react';
-import dataList from "./data.json" 
+import expensData from "./expensData.json";
+import incomeData from "./incomeData.json";
 import { useForm } from "react-hook-form";
 import { v4 as uuid4 } from "uuid";
 
+
+const useInput = (initialValue)=>{
+    const [value, setValue]=useState(initialValue);
+
+    const reset=()=>{
+        setValue(initialValue);
+    };
+    const handleClick =(e)=>{
+        setValue(e.target.value);
+    };
+
+    return [value, handleClick, reset];
+}
+
+const sumFun=(initVal)=>{
+    expensData.map((det, index)=>(
+        <li key={index}>
+            {det.amount}
+        </li>
+    ))
+}
+
+
 function Expense(){
 
+    const [expens, setExpense] =useState(expensData);
+    const [income, setIncome] =useState(incomeData);
+    const [expensIn, setExpenseIn] =useState([]);
+    const [cost, handleSetCost, resetCost]=useInput("");
+    const [licz, setLicz]=useState("");
     
-    const Expens=[{
-        id:"1",
-        name:"radio",
-        amount:"80",
-        category:"audio"
-        },
-        {
-        id:"2",
-        name:"glosniki",
-        amount:"150",
-        category:"audio"
-        },
-        {
-        id:"3",
-        name:"kable",
-        amount:"10",
-        category:"audio"
-        }];
     
-    const Income=[{
-        id:"1",
-        name:"wyplata",
-        amount:"2000",
-        category:"etat"
-    }];
-
-    const [expens, setExpense] =useState(Expens);
-
     const {register, handleSubmit} = useForm();
+    
+
     const sendForm = (d) =>{
+        
         if(radio1==="wydatek"){
             const expenseInit={
                 id: uuid4(),
-                //type:radio1
+                type:radio1,
+                name:d.name,
+                amount:parseInt(d.amount),
+                category:d.category
+            };
+           // sumFun(d.amount);
+            setExpenseIn([...expensIn+ d.amount]);
+            expensData.push(expenseInit);
+            setLicz(licz+expenseInit.amount);
+            sumFun(d);
+            console.log(expensData);
+            setRadio1(false);
+            setRadio2(false);
+        }else{
+            const incomeInit={
+                id: uuid4(),
+                type:radio2,
                 name:d.name,
                 amount:d.amount,
                 category:d.category
             };
-            dataList.push(expenseInit);
-            console.log(dataList);
+            incomeData.push(incomeInit);
+            console.log(incomeData);
             setRadio1(false);
             setRadio2(false);
         }
-    }
-        
+        };
 
     const [radio1, setRadio1] =useState(false);
     const [radio2, setRadio2] =useState(false);
@@ -71,7 +91,6 @@ function Expense(){
         { value:"paliwo", label:"paliwo"}
     ]
 
-     
     return(
             <form onSubmit={handleSubmit(sendForm)}>
                 <div>
@@ -104,6 +123,10 @@ function Expense(){
                         {...register("amount",{
                             //required:true,
                         })}
+                        value={cost}
+                        onChange={handleSetCost}
+                        type="number"
+                        
                     />
                 </div>
                 <div>
@@ -118,35 +141,19 @@ function Expense(){
                 <div>
                     <button
                     type="submit" value="submit"
-                    >Przelicz</button>
+                    >dodaj</button>
                 </div>
                 <div>
                     <p>
-                        {Expens.map(( e)=>{ return  [e.name, e.amount, e.category].join(", ")+" + " })}
-                    </p>
-                    <p>
-                        {Income.map((e)=>{ return [e.name, e.amount, e.category].join(", ") })}
-                    </p>
-                    <p>
-                        {dataList.map((e)=>{ return [e.name, e.amount, e.category].join(", ") })}
-                    </p>
-                    <p>
+                        {licz}
                         <h3>wydatki</h3>
-                        {Expens.map((det, index)=>(
-                            <li key={index}>
+                        {expens.map((det, index)=>(
+                            <li key={index} >
                                 <span>{det.name}</span>: {det.amount}{" "}
                             </li>
                         ))}
                         <h3>przychody</h3>
-                        {Income.map((det, index)=>(
-                            <li key={index}>
-                                <span>{det.name}</span>: {det.amount}{" "}
-                            </li>
-                        ))}
-                    </p>
-                    <p>
-                    <h3>przychody</h3>
-                        {dataList.map((det, index)=>(
+                        {income.map((det, index)=>(
                             <li key={index}>
                                 <span>{det.name}</span>: {det.amount}{" "}
                             </li>
